1815b3b6a5aa6f5a91dafdf5aa116c3f0c9327a0
plodah <mleiper89@gmail.com>
2024-07-12 22:39:01 +0100

    fix ragequit to release esc properly

--- users/plodah/plodah.c
+++ users/plodah/plodah.c
@@ -20,7 +20,7 @@ enum {
 };
 */
 
-void rage_quit(tap_dance_state_t *state, void *user_data) {
+void rage_quit_fin(tap_dance_state_t *state, void *user_data) {
     if (state->count >= 7) {
         if(IS_LAYER_ON(BORING_LAYER)) {
             SEND_STRING("Cool.");
@@ -39,12 +39,33 @@ void rage_quit(tap_dance_state_t *state, void *user_data) {
             register_code(KC_ESC);
         }
     }
-    reset_tap_dance(state);
+    //reset_tap_dance(state);
+};
+void rage_quit_rst(tap_dance_state_t *state, void *user_data) {
+    if (state->count >= 7) {
+        if(IS_LAYER_ON(BORING_LAYER)) {
+            //SEND_STRING("Cool.");
+            //layer_move(0);
+        }
+        else {
+            //SEND_STRING("I'm sorry!");
+            //layer_move(BORING_LAYER);
+        }
+    }
+    else{
+        if(state->count >= 3) {
+            //SEND_STRING("Nearies");
+        }
+        else{
+            unregister_code(KC_ESC);
+        }
+    }
+    //reset_tap_dance(state);
 };
 
 
 tap_dance_action_t tap_dance_actions[] = {
-    [TD_ESC_RAGEQUIT] = ACTION_TAP_DANCE_FN(rage_quit)
+    [TD_ESC_RAGEQUIT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, rage_quit_fin, rage_quit_rst)
 };
 #endif
 
