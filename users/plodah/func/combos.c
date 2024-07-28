ea0b4e9384c2f3c32dc5adb34c2621856543b518
plodah <mleiper89@gmail.com>
2024-07-28 02:14:39 +0100

    rename combos pl_combo_xyz

--- users/plodah/func/combos.c
+++ users/plodah/func/combos.c
@@ -4,41 +4,41 @@
     //===========================//
     //       COMMON COMBOS       //
     //===========================//
-    const uint16_t PROGMEM qa_combo[] = {KC_Q , KC_A, COMBO_END};
-    const uint16_t PROGMEM bsz_combo[] = {KC_NUBS, KC_Z, COMBO_END};
-    const uint16_t PROGMEM zx_combo[] = {KC_Z , KC_X, COMBO_END};
-    const uint16_t PROGMEM xc_combo[] = {KC_X , KC_C, COMBO_END};
-    const uint16_t PROGMEM cv_combo[] = {KC_C , KC_V, COMBO_END};
-    const uint16_t PROGMEM xcv_combo[] = {KC_X , KC_C , KC_V, COMBO_END};
-    const uint16_t PROGMEM tabq_combo[] = {KC_TAB , KC_Q , COMBO_END};
-    const uint16_t PROGMEM tabw_combo[] = {KC_TAB , KC_W , COMBO_END};
+    const uint16_t PROGMEM pl_combo_qa[] = {KC_Q , KC_A, COMBO_END};
+    const uint16_t PROGMEM pl_combo_bsz[] = {KC_NUBS, KC_Z, COMBO_END};
+    const uint16_t PROGMEM pl_combo_zx[] = {KC_Z , KC_X, COMBO_END};
+    const uint16_t PROGMEM pl_combo_xc[] = {KC_X , KC_C, COMBO_END};
+    const uint16_t PROGMEM pl_combo_cv[] = {KC_C , KC_V, COMBO_END};
+    const uint16_t PROGMEM pl_combo_xcv[] = {KC_X , KC_C , KC_V, COMBO_END};
+    const uint16_t PROGMEM pl_combo_tabq[] = {KC_TAB , KC_Q , COMBO_END};
+    const uint16_t PROGMEM pl_combo_tabw[] = {KC_TAB , KC_W , COMBO_END};
 
     //===========================//
     // COMBOS FOR DYNAMIC MACROS //
     //===========================//
 #   if defined(DYNAMIC_MACRO_ENABLE)
-        const uint16_t PROGMEM _12_combo[] = {KC_1 , KC_2, COMBO_END};
-        const uint16_t PROGMEM _13_combo[] = {KC_1 , KC_3, COMBO_END};
-        const uint16_t PROGMEM qw_combo[] = {KC_Q , KC_W, COMBO_END};
-        const uint16_t PROGMEM qe_combo[] = {KC_Q , KC_E, COMBO_END};
-        const uint16_t PROGMEM _3e_combo[] = {KC_3 , KC_E, COMBO_END};
+        const uint16_t PROGMEM pl_combo_12[] = {KC_1 , KC_2, COMBO_END};
+        const uint16_t PROGMEM pl_combo_13[] = {KC_1 , KC_3, COMBO_END};
+        const uint16_t PROGMEM pl_combo_qw[] = {KC_Q , KC_W, COMBO_END};
+        const uint16_t PROGMEM pl_combo_qe[] = {KC_Q , KC_E, COMBO_END};
+        const uint16_t PROGMEM pl_combo_3e[] = {KC_3 , KC_E, COMBO_END};
 #   endif //DYNAMIC_MACRO_ENABLE
 
     combo_t key_combos[] = {
-        COMBO(qa_combo, C(KC_A)),
-        COMBO(bsz_combo, C(KC_Z)),
-        COMBO(zx_combo, C(KC_X)),
-        COMBO(xc_combo, C(KC_C)),
-        COMBO(cv_combo, C(KC_V)),
-        COMBO(xcv_combo, C(S(KC_V))),
-        COMBO(tabq_combo, PL_ALTSTAB),
-        COMBO(tabw_combo, PL_ALTTAB),
+        COMBO(pl_combo_qa, C(KC_A)),
+        COMBO(pl_combo_bsz, C(KC_Z)),
+        COMBO(pl_combo_zx, C(KC_X)),
+        COMBO(pl_combo_xc, C(KC_C)),
+        COMBO(pl_combo_cv, C(KC_V)),
+        COMBO(pl_combo_xcv, C(S(KC_V))),
+        COMBO(pl_combo_tabq, PL_ALTSTAB),
+        COMBO(pl_combo_tabw, PL_ALTTAB),
 #       if defined(DYNAMIC_MACRO_ENABLE)
-            COMBO(_12_combo, DM_PLY1),
-            COMBO(_13_combo, DM_REC1),
-            COMBO(qw_combo, DM_PLY2),
-            COMBO(qe_combo, DM_REC2),
-            COMBO(_3e_combo, DM_RSTP),
+            COMBO(pl_combo_12, DM_PLY1),
+            COMBO(pl_combo_13, DM_REC1),
+            COMBO(pl_combo_qw, DM_PLY2),
+            COMBO(pl_combo_qe, DM_REC2),
+            COMBO(pl_combo_3e, DM_RSTP),
 #       endif //DYNAMIC_MACRO_ENABLE
     };
 
@@ -46,17 +46,17 @@
     //        MOUSE COMBOS       //
     //===========================//
 #else // ! defined(IS_MOUSE)
-        const uint16_t PROGMEM ms1ms2_combo[] = {KC_BTN1 , KC_BTN2, COMBO_END};
-        const uint16_t PROGMEM ms1ms4_combo[] = {KC_BTN1 , KC_BTN4, COMBO_END};
-        const uint16_t PROGMEM ms2ms4_combo[] = {KC_BTN2 , KC_BTN4, COMBO_END};
-        const uint16_t PROGMEM ms1ms5_combo[] = {KC_BTN1 , KC_BTN5, COMBO_END};
-        const uint16_t PROGMEM ms2ms5_combo[] = {KC_BTN2 , KC_BTN5, COMBO_END};
+        const uint16_t PROGMEM pl_combo_ms12[] = {KC_BTN1 , KC_BTN2, COMBO_END};
+        const uint16_t PROGMEM pl_combo_ms14[] = {KC_BTN1 , KC_BTN4, COMBO_END};
+        const uint16_t PROGMEM pl_combo_ms24[] = {KC_BTN2 , KC_BTN4, COMBO_END};
+        const uint16_t PROGMEM pl_combo_ms15[] = {KC_BTN1 , KC_BTN5, COMBO_END};
+        const uint16_t PROGMEM pl_combo_ms25[] = {KC_BTN2 , KC_BTN5, COMBO_END};
 
         combo_t key_combos[] = {
-            COMBO(ms1ms2_combo, DRAG_SCROLL),
-            COMBO(ms1ms4_combo, DRAG_SCROLL),
-            COMBO(ms2ms4_combo, DRAG_SCROLL),
-            COMBO(ms1ms5_combo, C(KC_C)),
-            COMBO(ms2ms5_combo, C(KC_V)),
+            COMBO(pl_combo_ms12, DRAG_SCROLL),
+            COMBO(pl_combo_ms14, DRAG_SCROLL),
+            COMBO(pl_combo_ms24, DRAG_SCROLL),
+            COMBO(pl_combo_ms15, C(KC_C)),
+            COMBO(pl_combo_ms25, C(KC_V)),
         };
 #endif // ! defined(IS_MOUSE)
