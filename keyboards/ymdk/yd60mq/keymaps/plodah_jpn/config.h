f502ef69f62c6a662e71f426147a08629f215ea1
plodah <mleiper89@gmail.com>
2024-07-30 00:40:31 +0100

    Add enter for left hand. Rearrange layers - boring on 1

--- keyboards/ymdk/yd60mq/keymaps/plodah_jpn/config.h
+++ keyboards/ymdk/yd60mq/keymaps/plodah_jpn/config.h
@@ -1,6 +1,6 @@
 #pragma once
 
 #define ENABLE_COMPILE_KEYCODE
-#define PLODAH_BORING_LAYER 3
+#define PLODAH_BORING_LAYER 1
 
 #define PLODAH_ALT_TAB_DELAY 750
--- keyboards/ymdk/yd60mq/keymaps/plodah_splbl_c/config.h
+++ keyboards/ymdk/yd60mq/keymaps/plodah_splbl_c/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define ENABLE_COMPILE_KEYCODE
-#define PLODAH_BORING_LAYER 4
+#define PLODAH_BORING_LAYER 1
 
 #define PLODAH_ALT_TAB_DELAY 750
 
