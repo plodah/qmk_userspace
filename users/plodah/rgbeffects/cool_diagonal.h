2524c26d51021fb29b55f12585f45e6badd5d423
plodah <mleiper89@gmail.com>
2024-08-14 00:52:44 +0100

    custom rgb effects formatting

--- users/plodah/rgbeffects/cool_diagonal.h
+++ users/plodah/rgbeffects/cool_diagonal.h
@@ -3,15 +3,12 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 RGB_MATRIX_EFFECT(COOL_DIAGONAL)
-
 #ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
-
-static HSV COOL_DIAGONAL_math(HSV hsv, uint8_t i, uint8_t time) {
+  static HSV COOL_DIAGONAL_math(HSV hsv, uint8_t i, uint8_t time){
     hsv.h = (g_led_config.point[i].x / 4) - g_led_config.point[i].y - time;
     return hsv;
-}
-
-bool COOL_DIAGONAL(effect_params_t* params) {
+  }
+  bool COOL_DIAGONAL(effect_params_t* params){
     return effect_runner_i(params, &COOL_DIAGONAL_math);
-}
+  }
 #endif // RGB_MATRIX_CUSTOM_EFFECT_IMPLS
