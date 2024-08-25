6d9349844541845dbd731e50873f29df22869894
plodah <mleiper89@gmail.com>
2024-08-25 19:27:16 +0100

    dynamic_macro_end_user seems to require returning bool after 2024q3

--- users/plodah/plodah.c
+++ users/plodah/plodah.c
@@ -161,17 +161,19 @@ void matrix_scan_user(void) {
 //      DYNAMIC MACROS      //
 //==========================//
 #if defined(DYNAMIC_MACRO_ENABLE)
-  void dynamic_macro_record_start_user(int8_t direction) {
+  bool dynamic_macro_record_start_user(int8_t direction) {
     #if defined (RGB_MATRIX_ENABLE) && defined (PLODAH_DMAC_INDIC_INDEX)
       is_dynamic_macro_recording = true;
     #endif
     # if defined(PLODAH_DYNAMIC_MACRO_TIMEOUT)
-        dynamic_macro_loop_timer = timer_read();
+      dynamic_macro_loop_timer = timer_read();
     # endif // PLODAH_DYNAMIC_MACRO_TIMEOUT
+    return true;
   }
-  void dynamic_macro_record_end_user(int8_t direction) {
+  bool dynamic_macro_record_end_user(int8_t direction) {
     #if defined (RGB_MATRIX_ENABLE) && defined (PLODAH_DMAC_INDIC_INDEX)
       is_dynamic_macro_recording = false;
     #endif
+    return true;
   }
 #endif // DYNAMIC_MACRO_ENABLE
