6a8cb508c050fad40554c264b996ac697f4f0f8f
plodah <127351418+plodah@users.noreply.github.com>
2024-11-01 15:03:01 +0000

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -eEuo pipefail
-mkdir -p /workspaces/qmk_firmware /workspaces/via_userspace /workspaces/via_keyboards /workspaces/qmk_firmware_upstream
+# mkdir -p /workspaces/qmk_firmware /workspaces/via_userspace /workspaces/via_keyboards /workspaces/qmk_firmware_upstream
 
 wget https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
@@ -18,13 +18,13 @@ git submodule update --init --recursive
 [ -d /workspaces/qmk_firmware ] || git clone https://github.com/plodah/qmk_firmware.git -b mybuilds-2024-09 /workspaces/qmk_firmware
 git config --global --add safe.directory /workspaces/qmk_firmware
 
-[ -d /workspaces/via_userspace ] || git clone https://github.com/the-via/qmk_userspace_via.git -b main /workspaces/via_userspace
-git config --global --add safe.directory /workspaces/via_userspace
-
 qmk config user.qmk_home=/workspaces/qmk_firmware
 qmk config user.overlay_dir="$userspacePath"
 
 qmk git-submodule
+
+[ -d /workspaces/via_userspace ] || git clone https://github.com/the-via/qmk_userspace_via.git -b main /workspaces/via_userspace
+git config --global --add safe.directory /workspaces/via_userspace
 
 [ -d /workspaces/via_keyboards ] || git clone https://github.com/the-via/keyboards.git -b master /workspaces/via_keyboards
 git config --global --add safe.directory /workspaces/via_keyboards
