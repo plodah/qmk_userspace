9d984ae538201c43004ff1be9ba66b9bb5bbcad5
plodah <127351418+plodah@users.noreply.github.com>
2024-11-01 13:31:06 +0000

    Add via repos to dev container

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -17,6 +17,12 @@ git submodule update --init --recursive
 [ -d /workspaces/qmk_firmware ] || git clone https://github.com/plodah/qmk_firmware.git -b mybuilds-2024-09 /workspaces/qmk_firmware
 git config --global --add safe.directory /workspaces/qmk_firmware
 
+[ -d /workspaces/via_userspace ] || git clone https://github.com/the-via/qmk_userspace_via.git -b main /workspaces/via-userspace
+git config --global --add safe.directory /workspaces/via-userspace
+
+[ -d /workspaces/via_userspace ] || git clone https://github.com/the-via/keyboards.git -b master /workspaces/via-keyboards
+git config --global --add safe.directory /workspaces/via-keyboards
+
 qmk config user.qmk_home=/workspaces/qmk_firmware
 qmk config user.overlay_dir="$userspacePath"
 
