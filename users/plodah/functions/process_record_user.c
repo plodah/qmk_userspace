9749abc4f59c63d80c9324ffff88a642fb6c321c
plodah <mleiper89@gmail.com>
2024-11-02 01:39:37 +0000

    return true on RGB_ keycodes

--- users/plodah/functions/process_record_user.c
+++ users/plodah/functions/process_record_user.c
@@ -101,49 +101,49 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         if (record->event.pressed) {
           repeathold_rgb_timer = timer_read();
         }
-        return false;
+        return true;
       case RGB_VAI:
         rgb_vai_pressed = record->event.pressed;
         if (record->event.pressed) {
           repeathold_rgb_timer = timer_read();
         }
-        return false;
+        return true;
       case RGB_HUD:
         rgb_hud_pressed = record->event.pressed;
         if (record->event.pressed) {
           repeathold_rgb_timer = timer_read();
         }
-        return false;
+        return true;
       case RGB_HUI:
         rgb_hui_pressed = record->event.pressed;
         if (record->event.pressed) {
           repeathold_rgb_timer = timer_read();
         }
-        return false;
+        return true;
       case RGB_SAD:
         rgb_sad_pressed = record->event.pressed;
         if (record->event.pressed) {
           repeathold_rgb_timer = timer_read();
         }
-        return false;
+        return true;
       case RGB_SAI:
         rgb_sai_pressed = record->event.pressed;
         if (record->event.pressed) {
           repeathold_rgb_timer = timer_read();
         }
-        return false;
+        return true;
       case RGB_SPD:
         rgb_spd_pressed = record->event.pressed;
         if (record->event.pressed) {
           repeathold_rgb_timer = timer_read();
         }
-        return false;
+        return true;
       case RGB_SPI:
         rgb_spi_pressed = record->event.pressed;
         if (record->event.pressed) {
           repeathold_rgb_timer = timer_read();
         }
-        return false;
+        return true;
     #endif // defined(PLODAH_REPEATHOLD_RGB) && defined(RGB_MATRIX_ENABLE)
   }
   return true;
