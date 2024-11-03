05186e1a5a0d97792624503859de342516626058
plodah <mleiper89@gmail.com>
2024-11-03 05:46:18 +0000

    dz60v2 is a keyboard.

---
+++ keyboards/dztech/dz60v2/keymaps/plodah/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define PLODAH_FEAT_KC
+#define PLODAH_QRGB_KC
+
+#define PLODAH_BORING_LAYER 1
+
+#define PLODAH_MSJIGGLER_ENABLED
+
+#include "plodah.h"
---
+++ keyboards/dztech/dz60v2/keymaps/plodahc/config.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#define PLODAH_FEAT_KC
+#define PLODAH_QRGB_KC
+
+#ifdef PRODUCT
+  #undef PRODUCT
+#endif // PRODUCT
+#define PRODUCT "Keychron Q1v1 ISO Enc | plodahc"
+
+#define PLODAH_BORING_LAYER 1
+
+#define PLODAH_MSJIGGLER_ENABLED
+
+#define BOOTMAGIC_ROW 0
+#define BOOTMAGIC_COLUMN 0
+
+#ifdef DYNAMIC_KEYMAP_LAYER_COUNT
+  #undef DYNAMIC_KEYMAP_LAYER_COUNT
+#endif
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
+#include "plodah.h"
