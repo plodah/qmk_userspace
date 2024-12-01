0af3e29ac8abaab52d7951be61c7f1589435bb1a
plodah <mleiper89@gmail.com>
2024-12-01 15:53:40 +0000

    Remove misplaced F11 tap enable the tapdances

--- keyboards/keebio/iris/keymaps/plodahc/keymap.c
+++ keyboards/keebio/iris/keymaps/plodahc/keymap.c
@@ -28,7 +28,7 @@ enum {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-         TD(TD_ESC_RAGEQUIT), KC_1, KC_2, KC_3, KC_4, TD(TD_F5),                   KC_6,    KC_7,    KC_8,    KC_9, TD(TD_F11), KC_BSPC,
+         TD(TD_ESC_RAGEQUIT), KC_1, KC_2, KC_3, KC_4, TD(TD_F5),                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
          KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC,
         KC_LSFT,    KC_A,  P_SALT,  P_DCTL,  P_FSFT,    KC_G,                      KC_H,  P_JSFT,  P_KCTL,  P_LALT, KC_SCLN, KC_QUOT,
         KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, XXXXXXX, XXXXXXX,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
