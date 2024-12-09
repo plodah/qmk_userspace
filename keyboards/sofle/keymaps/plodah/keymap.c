d0f45cdc2656690eba3e07106bfba7e02e6c25bb
plodah <mleiper89@gmail.com>
2024-12-09 02:42:41 +0000

    some work on additional layers sofle

--- keyboards/sofle/keymaps/plodah/keymap.c
+++ keyboards/sofle/keymaps/plodah/keymap.c
@@ -12,9 +12,9 @@ enum sofle_layers {
 enum keymap_keycodes {
     PL_ALTTAB = QK_USER_0,
     PL_ALTSTAB,
-    PL_ENC_CCW,
-    PL_ENC_CW,
-    PL_ENC_PRS,
+    PL_EC_CCW,
+    PL_EC_CW,
+    PL_EC_PR,
     PL_MSJG,
     PL_QFTR,
     PL_QRGB,
@@ -59,11 +59,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_QWERTY] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    TD(TD_F5),                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
-  KC_NUBS,  HRM_AG, HRM_SA,  HRM_DC,  HRM_FS,  KC_G,                          KC_H,    HRM_JS,  HRM_KC,  HRM_LA,  HRM_CG,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  PL_ENC_PRS, PL_ENC_PRS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                    KC_LGUI, KC_LALT, KC_LCTL, P_ENTFB, P_SPCFA,     P_SPCFA, P_ENTFB, KC_RCTL, KC_RALT, KC_RGUI
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    TD(TD_F5),                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+  KC_NUBS,  HRM_AG, HRM_SA,  HRM_DC,  HRM_FS,  KC_G,                        KC_H,    HRM_JS,  HRM_KC,  HRM_LA,  HRM_CG,  KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    PL_EC_PR, PL_EC_PR, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                    KC_LGUI, KC_LALT, KC_LCTL, P_ENTFB, P_SPCFA,  P_SPCFA,  P_ENTFB, KC_RCTL, KC_RALT, KC_RGUI
 ),
 
 /*
@@ -82,11 +82,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
 */
 [_COLEMAK] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    TD(TD_F5),                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                          KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_DEL,
-  KC_NUBS,  KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                          KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  PL_ENC_PRS, PL_ENC_PRS, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                    KC_LGUI, KC_LALT, KC_LCTL, P_ENTFB, P_SPCFA,     P_SPCFA, P_ENTFB, KC_RCTL, KC_RALT, KC_RGUI
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    TD(TD_F5),                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                        KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_DEL,
+  KC_NUBS,  KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                        KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    PL_EC_PR, PL_EC_PR, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                    KC_LGUI, KC_LALT, KC_LCTL, P_ENTFB, P_SPCFA,  P_SPCFA,  P_ENTFB, KC_RCTL, KC_RALT, KC_RGUI
 ),
 
 /* RAISE
@@ -104,11 +104,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_FN_A] = LAYOUT(
-  KC_GRV,  _______, _______, _______, _______, _______,                       _______, _______, _______, _______, _______, KC_DEL,
-  _______, KC_INS,  KC_PSCR, KC_APP,  _______, _______,                       KC_PGUP, _______, KC_UP,   _______, _______, _______,
-  _______, _______, _______, _______, _______, KC_CAPS,                       KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
-  _______, _______, _______, _______, _______, _______,PL_ENC_PRS,PL_ENC_PRS, _______, _______, _______, _______, _______, _______,
-                    _______, _______, _______, _______, _______,   _______,   _______, _______, _______, _______
+  KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+  KC_CAPS, _______, _______, KC_UP,   _______, KC_F6,                       KC_F7,   _______, KC_UP,   _______, _______, _______,
+  _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,                     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
+  _______, _______, _______, _______, KC_LBRC, KC_LCBR, PL_EC_PR, PL_EC_PR, KC_RCBR, KC_RBRC, _______, _______, _______, _______,
+                    _______, _______, _______, _______, _______,  _______,  _______, _______, _______, KC_APP
 ),
 
 /* LOWER
@@ -126,11 +126,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_FN_B] = LAYOUT(
-  KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,
-  _______, KC_EXLM, KC_AT,   KC_HASH,  KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-  _______, KC_EQL,  KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR,PL_ENC_PRS,PL_ENC_PRS, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______,
-                    _______, _______, _______, _______, _______,   _______,   _______, _______, _______, _______
+  KC_GRV,  PL_QVER, PL_QRGB, _______, _______, PL_MSJG,                     PL_MSJG, _______, _______, QK_MAKE, PL_QFTR, KC_DEL,
+  _______, _______, _______, KC_PGUP, _______, _______,                     _______, _______, KC_PGUP, _______, KC_PSCR, _______,
+  _______, _______, KC_HOME, KC_PGDN, KC_END,  _______,                     _______, KC_HOME, KC_PGDN, KC_END,  _______, _______,
+  _______, _______, _______, _______, _______, _______, PL_EC_PR, PL_EC_PR, _______, _______, _______, _______, _______, _______,
+                    _______, _______, _______, _______, _______,  _______,  _______, _______, _______, KC_APP
 ),
 
 /* EMPTY
@@ -148,20 +148,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
 
 [_EMPTY] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                       _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______,                       _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______,                       _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______,   _______,   _______, _______, _______, _______, _______, _______,
-                    _______, _______, _______, _______, _______,   _______,   _______, _______, _______, _______
+  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______, _______, _______, _______,
+                    _______, _______, _______, _______, _______,  _______,  _______, _______, _______, _______
 ),
  */
 };
 
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [_QWERTY] = {ENCODER_CCW_CW(PL_ENC_CCW, PL_ENC_CW), ENCODER_CCW_CW(PL_ENC_CCW, PL_ENC_CW)},
-    [_COLEMAK] = {ENCODER_CCW_CW(PL_ENC_CCW, PL_ENC_CW), ENCODER_CCW_CW(PL_ENC_CCW, PL_ENC_CW)},
-    [_FN_A] = {ENCODER_CCW_CW(PL_ENC_CCW, PL_ENC_CW), ENCODER_CCW_CW(PL_ENC_CCW, PL_ENC_CW)},
-    [_FN_B] = {ENCODER_CCW_CW(PL_ENC_CCW, PL_ENC_CW), ENCODER_CCW_CW(PL_ENC_CCW, PL_ENC_CW)},
+    [_QWERTY] = {ENCODER_CCW_CW(PL_EC_CCW, PL_EC_CW), ENCODER_CCW_CW(PL_EC_CCW, PL_EC_CW)},
+    [_COLEMAK] = {ENCODER_CCW_CW(PL_EC_CCW, PL_EC_CW), ENCODER_CCW_CW(PL_EC_CCW, PL_EC_CW)},
+    [_FN_A] = {ENCODER_CCW_CW(PL_EC_CCW, PL_EC_CW), ENCODER_CCW_CW(PL_EC_CCW, PL_EC_CW)},
+    [_FN_B] = {ENCODER_CCW_CW(PL_EC_CCW, PL_EC_CW), ENCODER_CCW_CW(PL_EC_CCW, PL_EC_CW)},
 };
 #endif // defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
