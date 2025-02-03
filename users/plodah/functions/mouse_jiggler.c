e938db96034e1fa144175ac18e3b4bd1b9159237
plodah <mleiper89@gmail.com>
2025-02-03 23:44:48 +0000

    Mouse Jiggler off while active

--- users/plodah/functions/mouse_jiggler.c
+++ users/plodah/functions/mouse_jiggler.c
@@ -33,6 +33,12 @@
     }
   }
 
+  void jiggle_delay(void){
+    if( msJigMainToken != INVALID_DEFERRED_TOKEN ){
+        extend_deferred_exec(msJigMainToken, 15000 );
+    }
+  }
+
   // Deltas only work if the length of the array is a power of 2.
   static int8_t circledeltas[32] = {0,-1,-2,-2,-3,-3,-4,-4,-4,-4,-3,-3,-2,-2,-1,0,0,1,2,2,3,3,4,4,4,4,3,3,2,2,1,0};
   static int8_t subtledeltas[16] = {1,-1,1,1,-2,2,-2,-2,2,-2,2,2,-1,1,-1,-1};
--- users/plodah/functions/pointing_device.c
+++ users/plodah/functions/pointing_device.c
@@ -1,6 +1,11 @@
-#if defined(PLODAH_DRAGSCROLL) // any condition requiring a pointing_device_task_user
+#if defined(PLODAH_DRAGSCROLL) || (defined(PLODAH_MSJIGGLER_ENABLED) && defined(DEFERRED_EXEC_ENABLE)) // any condition requiring a pointing_device_task_user
   #pragma once
   report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+    #if defined(PLODAH_MSJIGGLER_ENABLED) && defined(DEFERRED_EXEC_ENABLE)
+      if( mouse_report.x != 0 || mouse_report.y != 0 ){
+        jiggle_delay();
+      }
+    #endif
     #if defined(PLODAH_DRAGSCROLL)
       mouse_report = plodah_dragscroll_pointingtask(mouse_report);
     #endif
