f3d29345a020d2a2251f924d62d6608dfcef3c8b
plodah <mleiper89@gmail.com>
2025-02-11 03:17:23 +0000

    Allow selected one-hand HRM shortcuts

--- users/plodah/functions/achordion.c
+++ users/plodah/functions/achordion.c
@@ -344,9 +344,21 @@
                                             keyrecord_t* tap_hold_record,
                                             uint16_t other_keycode,
                                             keyrecord_t* other_record) {
+
+    if(KC_1 <= other_keycode && other_keycode <= KC_0) { return true; }
     switch (tap_hold_keycode) {
       case HRM_HG:
         if (other_keycode == HRM_LA) { return true; }
+        break;
+      case HRM_FC:
+        if (other_keycode == HRM_SA) {
+            return true;
+        }
+      case HRM_AG:
+        if (other_keycode == KC_E) { return true; }
+        break;
+      case HRM_GG:
+        if (other_keycode == KC_D) { return true; }
         break;
       case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
         return true;
