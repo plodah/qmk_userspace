51a2a32514653d02cf693dc0b2196e352b8d08f3
plodah <mleiper89@gmail.com>
2025-02-13 02:41:16 +0000

    missing comma; q10 fails compile

--- keyboards/keychron/q10/iso_encoder/keymaps/plodahc/keymap.c
+++ keyboards/keychron/q10/iso_encoder/keymaps/plodahc/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         PL_ECPR, P_RAGEQ, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_DEL,
         MC_0,    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,             KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,
         MC_1,    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_PGUP,
-        MC_2,    P_BSPFB, HRM_AG,  HRM_SA,  HRM_DS,  HRM_FC,  HRM_GG            HRM_HG,  HRM_JC,  HRM_KS,  HRM_LA,  HRM_CG,  KC_QUOT, KC_NUHS, KC_ENT,  KC_PGDN,
+        MC_2,    P_BSPFB, HRM_AG,  HRM_SA,  HRM_DS,  HRM_FC,  HRM_GG,           HRM_HG,  HRM_JC,  HRM_KS,  HRM_LA,  HRM_CG,  KC_QUOT, KC_NUHS, KC_ENT,  KC_PGDN,
         KC_END,  KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,
         KC_HOME, KC_LCTL, KC_LGUI, KC_LALT,          P_SPCFA, KC_RALT,                   P_BSPFB, KC_RCTL,                            KC_LEFT, KC_DOWN, KC_RGHT
     ),
