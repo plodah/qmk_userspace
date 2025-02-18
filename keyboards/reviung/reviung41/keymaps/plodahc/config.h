55b592a91afa6a25e81c3646559ed1092df19c26
plodah <mleiper89@gmail.com>
2025-02-18 03:43:13 +0000

    day 1 eod reviung41 config

---
+++ keyboards/reviung/reviung41/keymaps/plodahc/config.h
@@ -0,0 +1,52 @@
+#pragma once
+
+// #define KC_PL_QFTR
+#define KC_PL_QLYR
+// #define KC_PL_QRGB
+#define KC_PL_QVER
+
+#define PLODAH_TASKSWITCH_ENABLE
+// #define PLODAH_TASKSWITCH_FORCE_NDE
+// #define PLODAH_DYNAMIC_MACRO_FORCE_NDE
+#define PLODAH_HRM_COMBOS
+
+#if defined(PRODUCT)
+  #undef PRODUCT
+#endif // PRODUCT
+#define PRODUCT "reviung41 | plodahc"
+
+#define ACHORDION_ENABLE
+#define ACHORDION_STREAK
+
+// #define PLODAH_BORING_LAYER _QWEB
+
+#if defined(DEFERRED_EXEC_ENABLE)
+  #define PLODAH_MSJIGGLER_ENABLE
+  #define PLODAH_MSJIGGLER_INTRO
+  // #define PLODAH_MSJIGGLER_INTRO_TIMEOUT 1200
+#endif // DEFERRED_EXEC_ENABLE
+
+#if defined(TAP_DANCE_ENABLE)
+  #define PLODAH_TAPDANCE_TAPHOLD_ENABLE
+#endif // TAP_DANCE_ENABLE
+
+#if defined(DYNAMIC_MACRO_ENABLE)
+  #define PLODAH_DYNAMIC_MACRO_KCS_ENABLE
+#endif // DYNAMIC_MACRO_ENABLE
+
+#define BOOTMAGIC_ROW 0
+#define BOOTMAGIC_COLUMN 0
+
+#if defined(DYNAMIC_KEYMAP_LAYER_COUNT)
+  #undef DYNAMIC_KEYMAP_LAYER_COUNT
+#endif
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+
+/* ------- RGB LIGHTING ------- */
+#ifdef RGBLIGHT_ENABLE
+  #include "configs/rgblight-effects-few.h"
+  #define PLODAH_REPEATHOLD_RGB
+  // #define PLODAH_REPEATHOLD_RATE 100
+#endif // RGBLIGHT_ENABLE
+
+#include "plodah.h"
---
+++ keyboards/reviung/reviung41/keymaps/plodahc/keymap.c
@@ -0,0 +1,126 @@
+/* Copyright 2020 gtips
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+enum layer_names {
+    _QWEA,
+    _FNA,
+    _FNB,
+    _SYM,
+    _RGB,
+    _BLANK,
+};
+
+enum keymap_keycodes {
+    PL_TSKA = QK_USER_0,
+    PL_TSKB,
+    PL_DMAC1,
+    PL_DMAC2,
+    // PL_DRAG_SCROLL_MOMENTARY,
+    // PL_DRAG_SCROLL_TOGGLE,
+    // PL_ECCC,
+    // PL_ECCW,
+    // PL_ECPR,
+    PL_MSJG,
+    // PL_QFTR,
+    PL_QLYR,
+    // PL_QRGB,
+    PL_QTAP,
+    PL_QVER,
+};
+
+enum {
+    TD_ESC_RAGEQUIT,
+    TD_F2,
+    TD_F4,
+    TD_F5,
+    TD_F11,
+    TD_F12,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_QWEA] = LAYOUT(
+    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_TAB,  KC_A,    KC_S,    HRM_DS,  HRM_FC,  HRM_GG,           HRM_HG,  HRM_JC,  HRM_KS,  KC_L,    KC_SCLN, KC_QUOT,
+    KC_NUBS, HRM_ZG,  KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  HRM_SLG, KC_RSFT,
+                                        KC_LALT, P_ENTFB, P_SPCFA, P_ENTFB, KC_RALT
+  ),
+
+  [_FNA] = LAYOUT(
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+    KC_CAPS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,            KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS,
+    _______, KC_MINS, KC_EQL,  _______, KC_LCBR, KC_LBRC,          KC_RBRC, KC_RCBR, _______, _______, _______, _______,
+                                         _______, _______, _______, _______, _______
+  ),
+
+  [_FNB] = LAYOUT(
+    _______, _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP,          KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_PSCR, KC_DEL,
+    _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,          KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_PIPE,
+    _______, KC_UNDS, KC_PLUS, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+                                        _______, _______, _______, _______, _______
+  ),
+
+  [_SYM] = LAYOUT(
+    XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, PL_MSJG,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PIPE,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, KC_UNDS, KC_PLUS, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                        _______, _______, XXXXXXX, _______, _______
+  ),
+
+  [_RGB] = LAYOUT(
+    UG_VALU, UG_SATU, UG_HUEU, UG_NEXT, XXXXXXX, UG_TOGG,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    UG_VALD, UG_SATD, UG_HUED, UG_PREV, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                         _______, _______, XXXXXXX, _______, _______
+  ),
+
+  [_BLANK] = LAYOUT(
+    _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+                                        _______, _______, _______, _______, _______
+  ),
+
+/*
+  [_LOWER] = LAYOUT(
+    _______,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,    KC_PERC,            KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_DEL,
+    _______,  KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,   KC_PIPE,            KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_GRV,   KC_TILD,
+    _______,  KC_ESC,   KC_LGUI,  KC_LALT,  KC_CAPS,   KC_DQUO,            KC_HOME,  KC_END,   KC_PGUP,  KC_PGDN,  KC_PSCR,  RSFT_T(KC_SPC),
+                                            _______,   _______,  KC_ENT,   _______,  _______
+  ),
+
+  [_RAISE] = LAYOUT(
+    _______,  KC_1,     KC_2,     KC_3,     KC_4,      KC_5,               KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL,
+    _______,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,   KC_BSLS,            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,
+    _______,  KC_ESC,   KC_RGUI,  KC_RALT,  KC_CAPS,   KC_QUOT,            KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
+                                            _______,   _______,  KC_BSPC,  _______,  _______
+  ),
+
+  [_ADJUST] = LAYOUT(
+    UG_VALU,   UG_SATU, UG_HUEU,  UG_NEXT,  XXXXXXX,   UG_TOGG,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    UG_VALD,   UG_SATD, UG_HUED,  UG_PREV,  XXXXXXX,   XXXXXXX,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,            QK_BOOT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+                                            _______,   _______,  XXXXXXX,  _______,  _______
+  ),
+*/
+
+};
+
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _FNA, _FNB, _SYM);
+}
+
