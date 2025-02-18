ca8e9e30a26871562455ed3ccd5c0c96a9760748
plodah <mleiper89@gmail.com>
2025-02-18 01:02:20 +0000

    Remove some Home row mods from dz60

--- keyboards/dztech/dz60v2/keymaps/plodahc/keymap.c
+++ keyboards/dztech/dz60v2/keymaps/plodahc/keymap.c
@@ -5,6 +5,7 @@ enum layers {
     _QWEB,
     _FNA,
     _FNB,
+    //_QWEHRM,
 };
 
 enum keymap_keycodes {
@@ -38,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWEA] = LAYOUT_60_b_iso(
         P_RAGEQ, KC_1,    P_F2,    KC_3,    P_F4,    P_F5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    P_F11,   P_F12,   XXXXXXX, KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
-        P_BSPFB, HRM_AG,  HRM_SA,  HRM_DS,  HRM_FC,  HRM_GG,  HRM_HG,  HRM_JC,  HRM_KS,  HRM_LA,  HRM_CG,  KC_QUOT, KC_NUHS, KC_ENT,
+        P_BSPFB, KC_A,    KC_S,    HRM_DS,  HRM_FC,  KC_G,    KC_H,    HRM_JC,  HRM_KS,  KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  P_SFTSL,          KC_UP,   KC_SLSH,
         KC_LCTL, KC_LGUI, KC_LALT,          P_SPCFA,          P_SPCFB,          P_SPCFA,          P_ALAPP, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
@@ -47,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT,          KC_UP,   KC_SLSH,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  P_FNA,   KC_SPC,  KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,           P_FNA,            KC_SPC,           KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_FNA] = LAYOUT_60_b_iso(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, KC_DEL,
@@ -63,6 +64,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, P_PSTU,  _______, NK_TOGG, _______, _______, _______, _______,          KC_VOLU, _______,
         _______, GU_TOGG, _______,          KC_SPC,           KC_SPC,           KC_SPC,           KC_APP,  _______, KC_HOME, KC_VOLD, KC_END
     ),
+    /*
+    [_QWEHRM] = LAYOUT_60_b_iso(
+        P_RAGEQ, KC_1,    P_F2,    KC_3,    P_F4,    P_F5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    P_F11,   P_F12,   XXXXXXX, KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
+        P_BSPFB, HRM_AG,  HRM_SA,  HRM_DS,  HRM_FC,  HRM_GG,  HRM_HG,  HRM_JC,  HRM_KS,  HRM_LA,  HRM_CG,  KC_QUOT, KC_NUHS, KC_ENT,
+        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  P_SFTSL,          KC_UP,   KC_SLSH,
+        KC_LCTL, KC_LGUI, KC_LALT,          P_SPCFA,          P_SPCFB,          P_SPCFA,          P_ALAPP, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+    */
     /*
     [4] = LAYOUT_60_b_iso(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, _______,
