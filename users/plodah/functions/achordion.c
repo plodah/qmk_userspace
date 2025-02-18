3121b43acf54a75837cce05abf540ae7504462dd
plodah <mleiper89@gmail.com>
2025-02-18 03:39:15 +0000

    additional achordion cases for tab and non-hrm kcs

--- users/plodah/functions/achordion.c
+++ users/plodah/functions/achordion.c
@@ -345,15 +345,16 @@
                                             uint16_t other_keycode,
                                             keyrecord_t* other_record) {
 
-    if(KC_1 <= other_keycode && other_keycode <= KC_0) { return true; }
+    if (KC_1 <= other_keycode && other_keycode <= KC_0) { return true; }
+    if (other_keycode == KC_TAB) { return true; }
     switch (tap_hold_keycode) {
       case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
         return true;
       case HRM_HG:
-        if (other_keycode == HRM_LA) { return true; }
+        if (other_keycode == KC_L || other_keycode == HRM_LA) { return true; }
         break;
       case HRM_FC:
-        if (other_keycode == HRM_SA) { return true; }
+        if (other_keycode == KC_S || other_keycode == HRM_SA) { return true; }
         break;
       case HRM_AG:
         if (other_keycode == KC_E) { return true; }
