18136288ae8828061169e8f706dcd914b0a280f7
plodah <mleiper89@gmail.com>
2025-03-02 02:13:57 +0000

    leader key combos

--- users/plodah/functions/combos.c
+++ users/plodah/functions/combos.c
@@ -123,6 +123,12 @@
       const uint16_t PROGMEM pl_combo_fg[]     = {KC_F,     KC_G,                   COMBO_END};
       const uint16_t PROGMEM pl_combo_gh[]     = {KC_G,     KC_H,                   COMBO_END};
       const uint16_t PROGMEM pl_combo_hj[]     = {KC_H,     KC_J,                   COMBO_END};
+
+      #if defined(LEADER_ENABLE)
+        const uint16_t PROGMEM pl_combo_sf[]     = {KC_S,     KC_F,                   COMBO_END};
+        const uint16_t PROGMEM pl_combo_jl[]     = {KC_J,     KC_L,                   COMBO_END};
+      #endif // defined(LEADER_ENABLE)
+
       const uint16_t PROGMEM pl_combo_bsz[]    = {KC_NUBS,  KC_Z,                   COMBO_END};
       const uint16_t PROGMEM pl_combo_vb[]     = {KC_V,    KC_B,                    COMBO_END};
 
@@ -155,6 +161,15 @@
         const uint16_t PROGMEM pl_combo_hj_hrm[]      = {KC_H,    HRM_JC, COMBO_END};
         const uint16_t PROGMEM pl_combo_hj_hrm2[]     = {HRM_HG,  KC_J,   COMBO_END};
         const uint16_t PROGMEM pl_combo_hj_hrm3[]     = {HRM_HG,  HRM_JC, COMBO_END};
+
+        #if defined(LEADER_ENABLE)
+          const uint16_t PROGMEM pl_combo_sf_hrm[]     = {HRM_SA,   HRM_FC, COMBO_END};
+          const uint16_t PROGMEM pl_combo_sf_hrm2[]    = {HRM_SA,   KC_F,   COMBO_END};
+          const uint16_t PROGMEM pl_combo_sf_hrm3[]    = {KC_S,     HRM_FC, COMBO_END};
+          const uint16_t PROGMEM pl_combo_jl_hrm[]     = {HRM_JC,   HRM_LA, COMBO_END};
+          const uint16_t PROGMEM pl_combo_jl_hrm2[]    = {HRM_JC,   KC_L,   COMBO_END};
+          const uint16_t PROGMEM pl_combo_jl_hrm3[]    = {KC_J,     HRM_LA, COMBO_END};
+        #endif // defined(LEADER_ENABLE)
 
         const uint16_t PROGMEM pl_combo_bsz_hrm[]     = {KC_NUBS, HRM_ZG, COMBO_END};
         const uint16_t PROGMEM pl_combo_bsz2_hrm[]    = {KC_LSFT, HRM_ZG, COMBO_END};
@@ -231,6 +246,12 @@
         COMBO(pl_combo_fg,    C(KC_G)    ),
         COMBO(pl_combo_gh,    C(KC_H)    ),
         COMBO(pl_combo_hj,    C(KC_J)    ),
+
+        #if defined(LEADER_ENABLE)
+          COMBO(pl_combo_sf,  QK_LEAD),
+          COMBO(pl_combo_jl,  QK_LEAD),
+        #endif // defined(LEADER_ENABLE)
+
         COMBO(pl_combo_bsz,   C(KC_Z)    ),
         COMBO(pl_combo_vb,    C(KC_B)    ),
       #endif
@@ -262,6 +283,15 @@
           COMBO(pl_combo_hj_hrm,      C(KC_J)),
           COMBO(pl_combo_hj_hrm2,     C(KC_J)),
           COMBO(pl_combo_hj_hrm3,     C(KC_J)),
+
+          #if defined(LEADER_ENABLE)
+            COMBO(pl_combo_sf_hrm,      QK_LEAD),
+            COMBO(pl_combo_sf_hrm2,     QK_LEAD),
+            COMBO(pl_combo_sf_hrm3,     QK_LEAD),
+            COMBO(pl_combo_jl_hrm,      QK_LEAD),
+            COMBO(pl_combo_jl_hrm2,     QK_LEAD),
+            COMBO(pl_combo_jl_hrm3,     QK_LEAD),
+          #endif // defined(LEADER_ENABLE)
 
           COMBO(pl_combo_bsz_hrm,     C(KC_Z)),
           COMBO(pl_combo_bsz2_hrm,    C(KC_Z)),
