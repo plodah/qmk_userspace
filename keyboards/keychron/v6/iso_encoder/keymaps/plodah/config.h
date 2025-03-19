949cd6cfb09c1084457cde08859a53c0e3bacd85
plodah <mleiper89@gmail.com>
2025-03-19 03:42:25 +0000

    incorrectly applied configs

--- keyboards/keychron/v6/iso_encoder/keymaps/plodah/config.h
+++ keyboards/keychron/v6/iso_encoder/keymaps/plodah/config.h
@@ -1,12 +1,10 @@
 #pragma once
 
-#if defined(COMMUNITY_MODULE_CONFIG_QUERY_ENABLE)
-    // #define CM_QFTR_DISABLED
-    #define CM_QLYR_DISABLED
-    #define CM_QTAP_DISABLED
-    // #define CM_QVER_DISABLED
-    // #define CM_QRGB_DISABLED
-#endif // COMMUNITY_MODULE_CONFIG_QUERY_ENABLE
+// #define CM_QFTR_DISABLED
+#define CM_QLYR_DISABLED
+#define CM_QTAP_DISABLED
+// #define CM_QVER_DISABLED
+// #define CM_QRGB_DISABLED
 
 #define ACHORDION_STREAK
 
--- keyboards/sofle/keymaps/plodah/config.h
+++ keyboards/sofle/keymaps/plodah/config.h
@@ -13,13 +13,11 @@
 #endif //ENCODER_RESOLUTION_RIGHT
 #define ENCODER_RESOLUTION_RIGHT 4
 
-#if defined(COMMUNITY_MODULE_CONFIG_QUERY_ENABLE)
-    // #define CM_QFTR_DISABLED
-    // #define CM_QLYR_DISABLED
-    // #define CM_QTAP_DISABLED
-    // #define CM_QVER_DISABLED
-    // #define CM_QRGB_DISABLED
-#endif // COMMUNITY_MODULE_CONFIG_QUERY_ENABLE
+// #define CM_QFTR_DISABLED
+// #define CM_QLYR_DISABLED
+// #define CM_QTAP_DISABLED
+// #define CM_QVER_DISABLED
+// #define CM_QRGB_DISABLED
 #define KC_PL_SCRL
 
 #define LAYER_NAMES { "Qwert", "Colem", "Fn A ", "Fn B ", "oQwer", "oCole", "oFn A", "oFn B", "Mouse", "Tenth" }
