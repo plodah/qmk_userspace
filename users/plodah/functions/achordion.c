7a27c0b89102fd3826b04a32d3feeefb4046213d
plodah <mleiper89@gmail.com>
2025-04-13 03:12:59 +0100

    achordion / chordal minor changes; console output

--- users/plodah/functions/achordion.c
+++ users/plodah/functions/achordion.c
@@ -6,28 +6,43 @@
         uint16_t other_keycode,
         keyrecord_t* other_record) {
 
-        // if (KC_1 <= other_keycode && other_keycode <= KC_0) { return true; }
-        // if (other_keycode == KC_TAB) { return true; }
-        // if (other_keycode == KC_BSPC) { return true; }
-        // if (other_keycode == KC_DEL) { return true; }
-        // if (other_keycode == KC_ESC) { return true; }
-        if (KC_1 <= other_keycode && other_keycode <= QK_BASIC_MAX) { return true; }
-
+        if (KC_1 <= other_keycode && other_keycode <= QK_BASIC_MAX) {
+            dprintf("chordal nonalpha\n");
+            return true;
+        }
         switch (tap_hold_keycode) {
             case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
+                dprintf("Achordion LT\n");
                 return true;
-            case HRM_HG:
-                if (other_keycode == KC_L || other_keycode == HRM_LA) { return true; }
-                break;
+            case QK_MOMENTARY ... QK_MOMENTARY_MAX:
+                dprintf("Achordion MO\n");
+                return true;
+            case QK_TAP_DANCE ... QK_TAP_DANCE_MAX:
+                dprintf("Achordion TD\n");
+                return true;
             case HRM_FC:
-                if (other_keycode == KC_S || other_keycode == HRM_SA) { return true; }
+                if (other_keycode == KC_S || other_keycode == HRM_SA) {
+                    dprintf("Achordion Ctrl S\n");
+                    return true;
+                }
                 break;
             case HRM_AG:
-                if (other_keycode == KC_E) { return true; }
-                break;
             case HRM_GG:
-                if (other_keycode == KC_D) { return true; }
+            case HRM_HG:
+                if (other_keycode == KC_E) {
+                    dprintf("Achordion Gui E\n");
+                    return true;
+                }
+                if (other_keycode == KC_L || other_keycode == HRM_LA) {
+                    dprintf("Achordion Gui L\n");
+                    return true;
+                }
+                if (other_keycode == KC_D || other_keycode == HRM_DS) {
+                    dprintf("Achordion Gui D\n");
+                    return true;
+                }
                 break;
+
         }
         return achordion_opposite_hands(tap_hold_record, other_record);
     }
--- users/plodah/functions/chordal_hold.c
+++ users/plodah/functions/chordal_hold.c
@@ -2,23 +2,45 @@
   #pragma once
   bool get_chordal_hold(uint16_t tap_hold_keycode, keyrecord_t* tap_hold_record,
     uint16_t other_keycode, keyrecord_t* other_record) {
-    if (KC_1 <= other_keycode && other_keycode <= QK_BASIC_MAX) { return true; }
+    if (KC_1 <= other_keycode && other_keycode <= QK_BASIC_MAX) {
+      dprintf("chordal nonalpha\n");
+      return true;
+    }
     switch (tap_hold_keycode) {
       case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
+        dprintf("chordal LT\n");
         return true;
-      case HRM_HG:
-        if (other_keycode == KC_L || other_keycode == HRM_LA) { return true; }
+      case QK_MOMENTARY ... QK_MOMENTARY_MAX:
+        dprintf("chordal MO\n");
+        return true;
+      case QK_TAP_DANCE ... QK_TAP_DANCE_MAX:
+        dprintf("chordal TD\n");
+        return true;
         break;
       case HRM_FC:
-        if (other_keycode == KC_S || other_keycode == HRM_SA) { return true; }
+        if (other_keycode == KC_S || other_keycode == HRM_SA) {
+            dprintf("chordal Ctrl S\n");
+            return true;
+        }
         break;
       case HRM_AG:
-        if (other_keycode == KC_E) { return true; }
-        break;
       case HRM_GG:
-        if (other_keycode == KC_D) { return true; }
+      case HRM_HG:
+        if (other_keycode == KC_E) {
+            dprintf("chordal Gui E\n");
+            return true;
+        }
+        if (other_keycode == KC_L || other_keycode == HRM_LA) {
+            dprintf("chordal Gui L\n");
+            return true;
+        }
+        if (other_keycode == KC_D || other_keycode == HRM_DS) {
+            dprintf("chordal Gui D\n");
+            return true;
+        }
         break;
     }
+    dprintf("chordal default\n");
     return get_chordal_hold_default(tap_hold_record, other_record);
   }
 #endif // CHORDAL_HOLD
