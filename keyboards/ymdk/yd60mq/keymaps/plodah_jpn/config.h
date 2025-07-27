f34b9e5d22811653b5fb8b50d3625b52a73ce1bf
plodah <mleiper89@gmail.com>
2025-07-27 15:18:17 +0100

    changes to dz60v2 layouts

--- keyboards/ymdk/yd60mq/keymaps/plodah_jpn/config.h
+++ keyboards/ymdk/yd60mq/keymaps/plodah_jpn/config.h
@@ -1,12 +1,35 @@
 #pragma once
 
 // #define CM_QFTR_DISABLED
-#define CM_QLYR_DISABLED
-#define CM_QTAP_DISABLED
+// #define CM_QLYR_DISABLED
+// #define CM_QTAP_DISABLED
 // #define CM_QVER_DISABLED
 // #define CM_QRGB_DISABLED
 
-#define PLODAH_BORING_LAYER 1
+#define CHORDAL_HOLD
+#define COMBOFLOW_ENABLE
+#define COMBOFLOW_TERM 110
+
+#define PLODAH_BORING_LAYER _QWEB
+
+#ifdef QUICK_TAP_TERM
+  #undef QUICK_TAP_TERM
+#endif
+#define QUICK_TAP_TERM 60
+
+#ifdef FLOW_TAP_TERM
+  #undef FLOW_TAP_TERM
+#endif
+#define FLOW_TAP_TERM 100
+
+#if defined(TAP_DANCE_ENABLE)
+  #define PLODAH_TAPDANCE_TAPHOLD_ENABLE
+#endif // TAP_DANCE_ENABLE
+
+#ifdef COMBO_ENABLE
+  #define PLODAH_HRM_COMBOS
+  #define PLODAH_BRM_COMBOS
+#endif
 
 /* ------- RGB LIGHTING ------- */
 #ifdef RGBLIGHT_ENABLE
--- keyboards/ymdk/yd60mq/keymaps/plodah_splbl/keymap.c
+++ keyboards/ymdk/yd60mq/keymaps/plodah_splbl/keymap.c
@@ -20,8 +20,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWEA] = LAYOUT_all(
         P_RAGEQ, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  XXXXXXX, KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, XXXXXXX,
-        P_BSPFB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,
-        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  P_SLSFT, XXXXXXX, KC_UP,   KC_SLSH,
+        P_BSPFB, HRM_AG,  HRM_SA,  HRM_DS,  HRM_FC,  HRM_GG,  HRM_HG,  HRM_JC,  HRM_KS,  HRM_LA,  HRM_CG,  KC_QUOT, KC_NUHS, KC_ENT,
+        KC_LSFT, KC_NUBS, BRM_ZG,  BRM_XA,  BRM_CS,  BRM_VC,  BRM_BG,  BRM_NG,  BRM_MC,  BRM_COS, BRM_DOA, P_SLSFT, XXXXXXX, KC_UP,   KC_SLSH,
         KC_LCTL, KC_LGUI, KC_LALT,          P_SPCFA,          P_SPCFB,          P_SPCFA,          KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_QWEB] = LAYOUT_all(
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, KC_DEL,
         KC_ENT,  KC_PGUP, KC_UP,   KC_PGDN, _______, _______, _______, _______, _______, _______, KC_PSCR, _______, _______, XXXXXXX,
         KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_ENT,  _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, KC_BTN1, KC_BTN1, KC_BTN1, KC_BTN1, KC_BTN1, KC_BTN1, KC_BTN1, _______, CW_TOGG, XXXXXXX, KC_PGUP, _______,
+        _______, _______, _______, KC_BTN1, KC_BTN1, KC_BTN1, KC_BTN1, KC_BTN1, KC_BTN1, KC_BTN1, _______, _______, XXXXXXX, KC_PGUP, _______,
         _______, GU_TOGG, _______,          KC_SPC,           KC_SPC ,          KC_SPC,           _______, _______, KC_HOME, KC_PGDN, KC_END
     ),
     [_FNB] = LAYOUT_all(
