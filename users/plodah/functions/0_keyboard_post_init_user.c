04ce27f91108a842396dd7ba7139e6e95ce4d54f
plodah <mleiper89@gmail.com>
2025-11-04 02:54:40 +0000

    fix include error final final thistime no wait final

--- users/plodah/functions/0_keyboard_post_init_user.c
+++ users/plodah/functions/0_keyboard_post_init_user.c
@@ -1,8 +1,12 @@
 #pragma once
-#include "debug.c"
-#include "ploopy_via.h"
 
 #if defined (CONSOLE_ENABLE) || (defined(VIA_ENABLE) && defined(PLOOPY_VIAMENUS))
+    #if defined(CONSOLE_ENABLE)
+      #include "debug.c"
+    #endif // defined(CONSOLE_ENABLE)
+    #if (defined(VIA_ENABLE) && defined(PLOOPY_VIAMENUS))
+      #include "ploopy_via.h"
+    #endif // (defined(VIA_ENABLE) && defined(PLOOPY_VIAMENUS))
     void keyboard_post_init_user(void) {
         #ifdef CONSOLE_ENABLE
             keyboard_post_init_user_debug();
