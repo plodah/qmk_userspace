cfed87f6bdb9ec7991703ade02c88cfebd1d1c39
plodah <mleiper89@gmail.com>
2025-11-16 04:35:00 +0000

    updated oled display stuff

--- users/plodah/functions/oled.c
+++ users/plodah/functions/oled.c
@@ -1,36 +1,65 @@
 #if defined(OLED_ENABLE)
     #pragma once
-    #include QMK_KEYBOARD_H
     #include "oled.h"
 
     #if defined(LAYER_NAMES)
         char *labels[DYNAMIC_KEYMAP_LAYER_COUNT] = LAYER_NAMES;
     #endif // defined(LAYER_NAMES)
 
-    static void layer_display( layer_state_t state ) {
+    void oled_layer_display ( void ) {
+        oled_write_P(PSTR("\n"), false);
+        #if defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
+            oled_write_P(PSTR("  "), false);
+        #endif // defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
         oled_write_P(PSTR("L:"), false);
-        oled_write_P(get_u8_str(get_highest_layer(state), ' '), false);
+        oled_write_P(get_u8_str(get_highest_layer(layer_state|default_layer_state), ' '), false);
+        #if defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
+            oled_write_P(PSTR("\n    "), false);
+        #endif // defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
+
         #if defined(LAYER_NAMES)
-            oled_write_P(labels[get_highest_layer(state)], false);
+            oled_write_P(labels[get_highest_layer(layer_state|default_layer_state)], false);
         #endif // defined(LAYER_NAMES)
+
     }
 
-    static void locks_display ( led_t state ) {
-        oled_write_P(PSTR("C"), state.caps_lock);
+    void oled_locks_display ( void ) {
+        oled_write_P(PSTR("\n"), false);
+        #if defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
+            oled_write_P(PSTR(" "), false);
+        #endif // defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
+        oled_write_P(PSTR("C"), host_keyboard_led_state().caps_lock);
         oled_write_P(PSTR(" "), false);
-        oled_write_P(PSTR("N"), state.num_lock);
+        #if defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
+            oled_write_P(PSTR(" "), false);
+        #endif // defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
+        oled_write_P(PSTR("N"), host_keyboard_led_state().num_lock);
         oled_write_P(PSTR(" "), false);
-        oled_write_P(PSTR("S"), state.scroll_lock);
+        #if defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
+            oled_write_P(PSTR(" "), false);
+        #endif // defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
+        oled_write_P(PSTR("S"), host_keyboard_led_state().scroll_lock);
     }
 
-    static void mods_display ( void ) {
+    void oled_mods_display ( void ) {
+        oled_write_P(PSTR("\n"), false);
+        #if defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
+            oled_write_P(PSTR(" "), false);
+        #endif // defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
         oled_write_P(PSTR("C"), (get_mods() & MOD_MASK_CTRL));
+        #if defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
+            oled_write_P(PSTR(" "), false);
+        #endif // defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
         oled_write_P(PSTR("S"), (get_mods() & MOD_MASK_SHIFT));
+        #if defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
+            oled_write_P(PSTR(" "), false);
+        #endif // defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
         oled_write_P(PSTR("A"), (get_mods() & MOD_MASK_ALT));
+        #if defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
+            oled_write_P(PSTR(" "), false);
+        #endif // defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
         oled_write_P(PSTR("G"), (get_mods() & MOD_MASK_GUI));
-        oled_write_P(PSTR(" "), false);
     }
-
 
     #if defined(AUTOCORRECT_ENABLE)
         const char *autocorrect_display_row( char* typo ) {
@@ -45,14 +74,17 @@
             strncpy(str2, str + 0, 10);
             str2[10] = '\0';
             return str2;
-            // oled_write_P(PSTR(autocorrect_display_row(recAcA)), false);
+            // oled_write(PSTR(autocorrect_display_row(recAcA)), false);
         }
 
-        void autocorrect_display(void) {
-            oled_write_P(PSTR("\n"), false);
-            oled_write_P(PSTR(autocorrect_display_row(recAcA)), false);
-            oled_write_P(PSTR(autocorrect_display_row(recAcB)), false);
-            oled_write_P(PSTR(autocorrect_display_row(recAcC)), false);
+        void oled_autocorrect_display(void) {
+            #if defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
+                oled_write(PSTR(" "), false);
+            #endif // defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
+            oled_write(PSTR("\n"), false);
+            oled_write(PSTR(autocorrect_display_row(recAcA)), false);
+            oled_write(PSTR(autocorrect_display_row(recAcB)), false);
+            oled_write(PSTR(autocorrect_display_row(recAcC)), false);
         }
 
         bool apply_autocorrect_oled(uint8_t backspaces, const char *str, char *typo, char *correct) {
@@ -70,29 +102,59 @@
         }
     #endif // defined(AUTOCORRECT_ENABLE)
 
-    static void render_sma_logo( bool invert ) {
-        static const char PROGMEM sma_logo[] = {
-            0x80, 0x81, 0x82, 0x83, 0x84, // 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94,
-            0xa0, 0xa1, 0xa2, 0xa3, 0xa4, // 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4,
-            0xc0, 0xc1, 0xc2, 0xc3, 0xc4, // 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4,
-            //0x00
-        };
-        oled_write_P(sma_logo, invert);
-    }
-
-    bool oled_task_user(void) {
+    __attribute__((weak)) bool oled_task_user(void) {
         if ( is_keyboard_master() ){
-            render_sma_logo(true);
-            locks_display(host_keyboard_led_state());
-            mods_display();
-            layer_display(layer_state);
+            #if defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
+                oled_write_P(logo_10x5, true);
+                // render_qmk_logo_10x5(false);
+            #else
+                oled_write_P(logo_5x5, true);
+                // render_qmk_logo_5x5(true);
+            #endif //
+            oled_locks_display();
+            oled_mods_display();
+            oled_layer_display();
             #if defined(AUTOCORRECT_ENABLE)
-              autocorrect_display();
+                oled_autocorrect_display();
             #endif // AUTOCORRECT_ENABLE
             return false;
         }
         else {
-            return true;
+            #if defined(SPLIT_LED_STATE_ENABLE) || defined(SPLIT_MODS_ENABLE) || defined(SPLIT_LAYER_STATE_ENABLE)
+                #if defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
+                    oled_write_P(logo_10x5, true);
+                    // render_qmk_logo_10x5(false);
+                #else
+                    oled_write_P(logo_5x5, true);
+                    // render_qmk_logo_5x5(true);
+                #endif //
+
+                #if defined(SPLIT_LED_STATE_ENABLE)
+                    oled_locks_display();
+                #endif // SPLIT_LED_STATE_ENABLE
+
+                #if defined(SPLIT_MODS_ENABLE)
+                    oled_mods_display();
+                #endif // SPLIT_MODS_ENABLE
+
+                #if defined(SPLIT_LAYER_STATE_ENABLE)
+                    oled_layer_display();
+                #endif // SPLIT_LAYER_STATE_ENABLE
+
+                oled_write_P(PSTR("\n\n"), false);
+                #if defined(OLED_DISPLAY_128X64) || defined(OLED_DISPLAY_64X128)
+                    oled_write_P(dual_logo_10x5, true);
+                    // render_qmk_logo_10x5(false);
+                #else
+                    oled_write_P(logo_5x5, true);
+                    // render_qmk_logo_5x5(true);
+                #endif //
+
+
+                return false;
+            #else
+              return true;
+            #endif //
         }
     }
 
