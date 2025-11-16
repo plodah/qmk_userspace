d313fd53b449d7e3d5908560bb227db405ffad16
plodah <mleiper89@gmail.com>
2025-11-16 04:20:45 +0000

    rgb_config cleanup

--- users/plodah/configs/rgbmatrix-config.h
+++ users/plodah/configs/rgbmatrix-config.h
@@ -2,62 +2,61 @@
 
 #ifdef RGB_MATRIX_ENABLE
 //-- RGB SETTINGS
-  #ifdef RGB_MATRIX_DEFAULT_MODE
+  #if defined(RGB_MATRIX_DEFAULT_MODE)
     #undef RGB_MATRIX_DEFAULT_MODE
   #endif
   #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_GRADIENT_LEFT_RIGHT
 
-  #ifdef RGB_MATRIX_DEFAULT_HUE
+  #if defined(RGB_MATRIX_DEFAULT_HUE)
     #undef RGB_MATRIX_DEFAULT_HUE
   #endif
   #define RGB_MATRIX_DEFAULT_HUE 79
 
-  #ifdef RGB_MATRIX_DEFAULT_SAT
+  #if defined(RGB_MATRIX_DEFAULT_SAT)
     #undef RGB_MATRIX_DEFAULT_SAT
   #endif
   #define RGB_MATRIX_DEFAULT_SAT 239
 
-  #ifdef RGB_MATRIX_DEFAULT_VAL
+  #if defined(RGB_MATRIX_DEFAULT_VAL)
     #undef RGB_MATRIX_DEFAULT_VAL
   #endif
   #define RGB_MATRIX_DEFAULT_VAL 127
 
-  #ifdef RGB_MATRIX_DEFAULT_SPD
+  #if defined(RGB_MATRIX_DEFAULT_SPD)
     #undef RGB_MATRIX_DEFAULT_SPD
   #endif
   #define RGB_MATRIX_DEFAULT_SPD 48
 
-  #ifdef RGB_MATRIX_SLEEP
-    #undef RGB_MATRIX_SLEEP
+  #if ! defined(RGB_MATRIX_SLEEP)
+    #define RGB_MATRIX_SLEEP
   #endif
-  #define RGB_MATRIX_SLEEP
 
-  #ifdef RGB_MATRIX_TIMEOUT
+  #if defined(RGB_MATRIX_TIMEOUT)
     #undef RGB_MATRIX_TIMEOUT
   #endif
   #define RGB_MATRIX_TIMEOUT 270000
 
-  #ifdef RGB_MATRIX_KEYRELEASES
+  #if defined(RGB_MATRIX_KEYRELEASES)
     #undef RGB_MATRIX_KEYRELEASES
   #endif
   // #define RGB_MATRIX_KEYRELEASES
 
-  #ifdef RGB_MATRIX_HUE_STEP
+  #if defined(RGB_MATRIX_HUE_STEP)
     #undef RGB_MATRIX_HUE_STEP
   #endif
   #define RGB_MATRIX_HUE_STEP 5
 
-  #ifdef RGB_MATRIX_SAT_STEP
+  #if defined(RGB_MATRIX_SAT_STEP)
     #undef RGB_MATRIX_SAT_STEP
   #endif
   #define RGB_MATRIX_SAT_STEP 8
 
-  #ifdef RGB_MATRIX_VAL_STEP
+  #if defined(RGB_MATRIX_VAL_STEP)
     #undef RGB_MATRIX_VAL_STEP
   #endif
   #define RGB_MATRIX_VAL_STEP 8
 
-  #ifdef RGB_MATRIX_SPD_STEP
+  #if defined(RGB_MATRIX_SPD_STEP)
     #undef RGB_MATRIX_SPD_STEP
   #endif
   #define RGB_MATRIX_SPD_STEP 8
