6baa2004d6f55dfaac67896e3d8b9bde9f43335f
plodah <mleiper89@gmail.com>
2026-01-02 01:18:59 +0000

    pre-delivery keymap for dasky/reverb

---
+++ keyboards/dasky/reverb/keymaps/plodah/config.h
@@ -0,0 +1,110 @@
+/*
+Copyright 2019 @foostan
+Copyright 2020 Drashna Jaelre <@drashna>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+// #define CM_QFTR_DISABLED
+// #define CM_QLYR_DISABLED
+// #define CM_QTAP_DISABLED
+// #define CM_QVER_DISABLED
+// #define CM_QRGB_DISABLED
+
+// #define TASKSWITCH_FORCE_NDE
+// #define PLODAH_DYNAMIC_MACRO_FORCE_NDE
+
+#define CHORDAL_HOLD
+#define COMBOFLOW_TERM 100
+#define LOCKING_SUPPORT_ENABLE
+#define TURBO_FIRE_KEY_A MS_BTN1
+
+#define PLODAH_DUAL_TRI_LAYER
+#define PLODAH_BORING_LAYER _O_QWERTY
+
+#ifdef QUICK_TAP_TERM
+  #undef QUICK_TAP_TERM
+#endif
+#define QUICK_TAP_TERM 60
+
+#ifdef FLOW_TAP_TERM
+  #undef FLOW_TAP_TERM
+#endif
+#define FLOW_TAP_TERM 100
+
+#define CAPS_LOCK_LED_INDEX 17
+#define PLODAH_CAPS_INDIC_HSV HSV_MAGENTA
+
+#define PLODAH_MODS_INDIC_LCTL_INDEX 13
+#define PLODAH_MODS_INDIC_LGUI_INDEX 16
+#define PLODAH_MODS_INDIC_LALT_INDEX 15
+#define PLODAH_MODS_INDIC_LSHIFT_INDEX 14
+#define PLODAH_MODS_INDIC_RCTL_INDEX 40
+#define PLODAH_MODS_INDIC_RGUI_INDEX 43
+#define PLODAH_MODS_INDIC_RALT_INDEX 42
+#define PLODAH_MODS_INDIC_RSHIFT_INDEX 41
+#define PLODAH_MODS_INDIC_HSV 240, 84, 255
+#define PLODAH_GULOCK_INDIC_ENABLE
+
+#define PLODAH_LAYER_INDIC_INDEXES { 4, 3, 2, 1, 0, 53, 52, 51, 50, 49 }
+#define PLODAH_LAYER_INDIC_HSV HSV_MAGENTA
+// #define PLODAH_LAYER_INDIC_ONZERO
+// #define PLODAH_LAYER_INDIC_BG_HSV HSV_ORANGE
+
+#ifdef DYNAMIC_MACRO_ENABLE
+  #define PLODAH_DMAC_INDIC_INDEX 47
+  #define PLODAH_DMAC_INDIC_HSV HSV_RED
+#endif
+
+#define PLODAH_TYPINGINDICATOR_RGBINDEX 48
+#define PLODAH_TYPINGINDICATOR_HSV 240, 84, 255
+#define PLODAH_TYPINGINDICATOR_DURATION 600
+
+#if defined(COMMUNITY_MODULE_MOUSE_JIGGLER_ENABLE)
+  // #define MSJIGGLER_NOINTRO
+  // #define MSJIGGLER_INTRO_TIMEOUT 1200
+  // #define MSJIGGLER_PATTERN 1
+  // #define MSJIGGLER_AUTOSTOP
+  #define MSJIGGLER_INDICATOR_RGBINDEX 35
+  #define MSJIGGLER_INDICATOR_HSV 0x0B, 0xFF, 0xFF
+#endif // COMMUNITY_MODULE_MOUSE_JIGGLER_ENABLE
+
+#if defined(TAP_DANCE_ENABLE)
+  #define PLODAH_TAPDANCE_TAPHOLD_ENABLE
+#endif // TAP_DANCE_ENABLE
+
+#ifdef COMBO_ENABLE
+  #define PLODAH_HRM_COMBOS
+  #define PLODAH_BRM_COMBOS
+#endif
+
+#if defined(DYNAMIC_MACRO_ENABLE)
+  #define PLODAH_DYNAMIC_MACRO_KCS_ENABLE
+#endif // DYNAMIC_MACRO_ENABLE
+
+#ifdef RGB_MATRIX_ENABLE
+  #define RGB_MATRIX_DEFAULT_VAL 42
+  #include "configs/rgbmatrix-effects-many.h"
+  #include "configs/rgbmatrix-config.h"
+  #define PLODAH_REPEATHOLD_RGB
+  // #define PLODAH_REPEATHOLD_RATE 100
+#endif // RGB_MATRIX_ENABLE
+
+#if defined(TRI_LAYER_ENABLE)
+  #define TRI_LAYER_UNSTRICT
+#endif // TRI_LAYER_ENABLE
+
+#include "plodah.h"
---
+++ keyboards/dasky/reverb/keymaps/plodah/keymap.c
@@ -0,0 +1,123 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+#include QMK_KEYBOARD_H
+
+enum layers {
+    _QWERTY,
+    _COLEMAK,
+    _FNA,
+    _FNB,
+    _FNC,
+    _O_QWERTY,
+    _LOWER,
+    _RAISE,
+    _ADJUST,
+};
+
+enum {
+    TD_ESC_RAGEQUIT,
+    TD_F2,
+    TD_F4,
+    TD_F5,
+    TD_F11,
+    TD_F12,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_QWERTY] = LAYOUT(
+    P_RAGEQ, KC_1,    P_F2,    KC_3,    P_F4,    P_F5,             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    P_BSCTL, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_TAB,  HRM_AG,  HRM_SA,  HRM_DS,  HRM_FC,  HRM_GG,           HRM_HG,  HRM_JC,  HRM_KS,  HRM_LA,  HRM_CG, KC_QUOT,
+    KC_NUBS, BRM_ZG,  BRM_XA,  BRM_CS,  BRM_VC,  BRM_BG,           BRM_NG,  BRM_MC,  BRM_COS, BRM_DOA, BRM_SLG, QK_REP,
+                               P_HOMFC, P_ENTFB, P_SPCFA,          P_SPCFA, P_ENTFB, P_ENDFC
+  ),
+
+  [_COLEMAK] = LAYOUT(
+    P_RAGEQ, KC_1,    P_F2,    KC_3,    P_F4,    P_F5,             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,             KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
+    KC_NUBS, HRM_AG,  HRM_RA,  HRM_SS,  HRM_TC,  HRM_DG,           HRM_HG,  HRM_NC,  HRM_ES,  HRM_IA,  HRM_OG,  KC_QUOT,
+    KC_LSFT, BRM_ZG,  KC_X,    KC_C,    KC_V,    KC_B,             KC_K,    KC_M,    KC_COMM, KC_DOT,  BRM_SLG, QK_REP,
+                               KC_LGUI, P_ENTFB, P_SPCFA,          P_SPCFA, P_ENTFB, KC_RGUI
+  ),
+
+  [_FNA] = LAYOUT(
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,            KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+    _______, _______, _______, _______, _______, KC_F6,            KC_F7,   _______, _______, _______, _______, KC_DEL,
+    KC_BSPC, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, KC_NUHS,
+    _______, KC_MINS, KC_EQL,  _______, KC_LCBR, KC_LBRC,          KC_RBRC, KC_RCBR, _______, _______, _______, QK_AREP,
+                               _______, _______, _______,          _______, _______, KC_APP
+  ),
+
+  [_FNB] = LAYOUT(
+    _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, KC_DEL,
+    _______, _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP,          KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_PSCR, KC_DEL,
+    KC_DEL,  _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,          KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_PIPE,
+    _______, KC_UNDS, KC_PLUS, SH_TOGG, _______, _______,          _______, _______, SH_TOGG, _______, _______, QK_AREP,
+                               _______, _______, _______,          _______, _______, KC_APP
+  ),
+
+  [_FNC] = LAYOUT(
+    KC_CAPS, CM_QVER, CM_QRGB, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, CM_QLYR, CM_QTAP, QK_MAKE, CM_QFTR, CM_MSJG,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    KC_VOLU, XXXXXXX, KC_LSCR, MS_BTN2, MS_BTN1, MS_BTN3,          XXXXXXX, KC_LCTL, KC_LSFT, KC_LALT, QK_MAKE, KC_VOLU,
+    KC_VOLD, CM_TTAT, XXXXXXX, MS_BTN4, MS_BTN5, KC_LSCR,          QWERTY,  COLEMAK, O_QWERT, XXXXXXX, XXXXXXX, KC_VOLD,
+                               _______, _______, _______,          _______, _______, _______
+  ),
+
+  [_O_QWERTY] = LAYOUT(
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,             KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_PSCR,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_ESC,
+                               KC_LGUI, LOWER,   KC_SPC,           KC_ENT,  RAISE,   KC_RALT
+  ),
+  [_LOWER] = LAYOUT(
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  KC_F12,
+    KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    KC_LCTL, _______, _______, _______, _______, _______,          KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______, _______,
+    KC_LSFT, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+                               _______, _______, _______,          KC_ENT,  _______, _______
+  ),
+  [_RAISE] = LAYOUT(
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR,
+    KC_TAB,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+    KC_LCTL, _______, _______, _______, _______, _______,          KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,
+    KC_LSFT, _______, _______, _______, _______, _______,          KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
+                               _______, _______, _______,          _______,  _______, _______
+  ),
+  [_ADJUST] = LAYOUT(
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR,
+    QK_BOOT, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+    RM_TOGG, RM_HUEU, RM_SATU, RM_VALU, _______, _______,          _______, _______, _______, _______, _______, _______,
+    RM_NEXT, RM_HUED, RM_SATD, RM_VALD, _______, _______,          _______, _______, _______, _______, _______, _______,
+                               _______, _______, _______,          _______,  _______, _______
+  ),
+
+/*
+  [_EMPTY] = LAYOUT(
+    _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+                               _______, _______, _______,          _______, _______, _______
+  ),
+*/
+
+};
+
+#ifdef SWAP_HANDS_ENABLE
+const keypos_t PROGMEM hand_swap_config[MATRIX_ROWS][MATRIX_COLS] = {
+    // Left   1 0 3 2 5 4 7 6
+    {{1, 5}, {0, 5}, {3, 5}, {2, 5}, {5, 5}, {4, 5}, {7, 5}, {6, 5}},
+    {{1, 4}, {0, 4}, {3, 4}, {2, 4}, {5, 4}, {4, 4}, {7, 4}, {6, 4}},
+    {{1, 3}, {0, 3}, {3, 3}, {2, 3}, {5, 3}, {4, 3}, {7, 3}, {6, 3}},
+
+    // Right  1 0 3 2 5 4 7 6
+    {{1, 2}, {0, 2}, {3, 2}, {2, 2}, {5, 2}, {4, 2}, {7, 2}, {6, 2}},
+    {{1, 1}, {0, 1}, {3, 1}, {2, 1}, {5, 1}, {4, 1}, {7, 1}, {6, 1}},
+    {{1, 0}, {0, 0}, {3, 0}, {2, 0}, {5, 0}, {4, 0}, {7, 0}, {6, 0}},
+
+    // Thumbs  5 4 3 2 1 0
+    {{5, 6}, {4, 6}, {3, 6}, {2, 6}, {1, 6}, {0, 6}}
+};
+#endif
