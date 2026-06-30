5e5b5be183ce9570aa503a8b81fec13daaba5ce9
plodah <github@plodah.uk>
2026-06-30 03:09:28 +0100

    move 6 functions to SRC += from daft #include's

--- users/plodah/rules.mk
+++ users/plodah/rules.mk
@@ -1,2 +1,9 @@
 INTROSPECTION_KEYMAP_C = plodah.c
 RGB_MATRIX_CUSTOM_USER = yes
+
+SRC += functions/chordal_hold.c
+SRC += functions/dipswitch.c
+SRC += functions/dynamic_macros.c
+SRC += functions/leader.c
+SRC += functions/oled.c
+SRC += functions/shutdown.c
